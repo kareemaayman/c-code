76d73a5636a685423fdcf3b53cc799dae5d8c723
Kareema Barouma <es-karima.ayman2027@alexu.edu.eg>
2024-09-07 01:11:47 +0300

    a folder found in home

---
+++ c/test_1/sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/// @brief 
+/// @param  
+int main()
+{   int num1=4,num2=5;
+    int sum;
+    printf("please enter numeber:");
+    scanf("%d",&num1);
+    printf("please enter another number:");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("the sum of %d and %d is %d",num1,num2,sum);
+}
